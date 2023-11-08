aceb89580d9b415b188cb580d190e69234e9ec03
rathod-0007 <rathodpavan2292@gmail.com>
2023-11-08 15:20:52 +0530

    Second commit

---
+++ itw.c
@@ -0,0 +1,12 @@
+#include <Stdio.h>
+int main()
+{
+    int age;
+    if (age < 18)
+        printf("You are eligible to enter the repository");
+
+    else
+    {
+        printf("You are denied");
+    }
+}
